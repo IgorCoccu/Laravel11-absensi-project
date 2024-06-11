d01df93a864cc3147463f80013992d94042ba735
igor_coccu <igorcoccu85@gmail.com>
2024-06-11 17:10:57 +0700

    ubah database Seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;  // Corrected the import statement
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,11 +14,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        User::factory(10)->create();
 
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'name' => 'Igor Coccu',
+            'email' => 'igor@felixindo.com',
+            'password' => Hash::make('1234567890'),
         ]);
     }
 }
